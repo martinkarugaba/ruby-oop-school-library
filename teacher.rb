f966a33ad920c0c0a85444eed5ea673f761b0dbb
Martin Karugaba <martinkarugaba21@gmail.com>
2023-08-02 12:47:26 +0300

    Create person, student and teacher classes

---
+++ teacher.rb
@@ -0,0 +1,10 @@
+class Teacher < Person
+  def initialize(id, age, specialization, name = "Unknown", parent_permission = true)
+    super(id, age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
